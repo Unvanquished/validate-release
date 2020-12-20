fa8564260399650b503dcaddee9fa9389ff5b0a8
slipher <slipher@protonmail.com>
2020-12-19 22:12:57 -0600

    Add test data set of ASLR-deficient binaries
    As usual the data is made from builds of include-bin with various flag
    tweaks. For Windows there are 3 variants: no DYNAMICBASE (daemon),
    DYNAMICBASE on but missing relocs (daemonded), and ASLR working, but no
    high-entropy VA (daemon-tty). I didn't manage to find a way to reproduce
    the missing relocs condition for 32-bit binaries, even though this
    happened in the 0.51 release.

--- testdata/make_zip.py
+++ testdata/make_zip.py
@@ -5,20 +5,23 @@ import os
 import zipfile
 
 dirname = os.path.dirname(__file__)
-location = os.path.join(dirname, 'passing-example_1.2')
-base = os.path.join(location, 'unvanquished_1.2')
-pkg = os.path.join(base, 'pkg')
-z = zipfile.ZipFile(os.path.join(dirname, 'passing-example_1.2.zip'), 'w')
-for x in os.listdir(pkg):
-    z.write(os.path.join(pkg, x), arcname='unvanquished_1.2/pkg/' + x)
+for testcase in ('passing-example_1.2', 'lacking-aslr_1.2'):
+    location = os.path.join(dirname, testcase)
+    base = os.path.join(location, 'unvanquished_1.2')
+    pkg = os.path.join(base, 'pkg')
+    z = zipfile.ZipFile(os.path.join(dirname, testcase + '.zip'), 'w')
+    for x in os.listdir(pkg):
+        z.write(os.path.join(pkg, x), arcname='unvanquished_1.2/pkg/' + x)
 
-for subdir in 'linux-amd64', 'macos-amd64', 'windows-i686', 'windows-amd64', 'symbols_1.2':
-    stream = io.BytesIO()
-    walk = os.path.join(base, subdir)
-    with zipfile.ZipFile(stream, 'w') as subzip:
-        for dirpath, _, filenames in os.walk(walk):
-            for filename in filenames:
-                filename = os.path.join(dirpath, filename)
-                subzip.write(filename, arcname=os.path.relpath(filename, walk))
-    stream.seek(0)
-    z.writestr('unvanquished_1.2/' + subdir + '.zip', stream.read())
+    for subdir in 'linux-amd64', 'macos-amd64', 'windows-i686', 'windows-amd64', 'symbols_1.2':
+        stream = io.BytesIO()
+        walk = os.path.join(base, subdir)
+        if not os.path.exists(walk):
+            continue
+        with zipfile.ZipFile(stream, 'w') as subzip:
+            for dirpath, _, filenames in os.walk(walk):
+                for filename in filenames:
+                    filename = os.path.join(dirpath, filename)
+                    subzip.write(filename, arcname=os.path.relpath(filename, walk))
+        stream.seek(0)
+        z.writestr('unvanquished_1.2/' + subdir + '.zip', stream.read())
