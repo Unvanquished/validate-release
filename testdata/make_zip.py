d6866be1c7516193dfbd5117b900fc44ca062b02
slipher <slipher@protonmail.com>
2021-03-02 12:13:50 -0600

    Refactor make_zip.py to be more general
    A directory with a name ending in .zip will be converted to a zip,
    recursively.
    
    Also take some measures to produce deterministic zip files, so that it
    will be possible to get matching md5sums with generated DPKs.

--- testdata/make_zip.py
+++ testdata/make_zip.py
@@ -9,26 +9,30 @@ def AddFile(zip, file, arcname):
         file = open(file, 'rb')
     info = zipfile.ZipInfo(arcname)
     info.external_attr = 0o100755 << 16
+    info.create_system = 3 # "Host OS" = "*nix"
     zip.writestr(info, file.read())
 
-dirname = os.path.dirname(__file__)
-for testcase in ('passing-example_1.2', 'lacking-aslr_1.2'):
-    location = os.path.join(dirname, testcase)
-    base = os.path.join(location, 'unvanquished_1.2')
-    pkg = os.path.join(base, 'pkg')
-    z = zipfile.ZipFile(os.path.join(dirname, testcase + '.zip'), 'w')
-    for x in os.listdir(pkg):
-        AddFile(z, os.path.join(pkg, x), 'unvanquished_1.2/pkg/' + x)
+def PopulateZip(zip, top):
+    for dir, dirnames, filenames in os.walk(top):
+        dirnames.sort() # Make deterministic zips
+        filenames.sort()
+        reldir = os.path.relpath(dir, top)
+        for filename in filenames:
+            filename = os.path.join(dir, filename)
+            AddFile(zip, filename, os.path.relpath(filename, top))
+        for i, dirname in reversed(list(enumerate(dirnames))):
+            if dirname.endswith('.dir'):
+                del dirnames[i]
+                dirname = os.path.join(dir, dirname)
+                stream = io.BytesIO()
+                with zipfile.ZipFile(stream, 'w') as subzip:
+                    PopulateZip(subzip, dirname)
+                stream.seek(0)
+                AddFile(zip, stream, os.path.relpath(dirname[:-4], top))
 
-    for subdir in 'linux-amd64', 'macos-amd64', 'windows-i686', 'windows-amd64', 'symbols_1.2':
-        stream = io.BytesIO()
-        walk = os.path.join(base, subdir)
-        if not os.path.exists(walk):
-            continue
-        with zipfile.ZipFile(stream, 'w') as subzip:
-            for dirpath, _, filenames in os.walk(walk):
-                for filename in filenames:
-                    filename = os.path.join(dirpath, filename)
-                    AddFile(subzip, filename, os.path.relpath(filename, walk))
-        stream.seek(0)
-        AddFile(z, stream, 'unvanquished_1.2/' + subdir + '.zip')
+testdata = os.path.dirname(os.path.join('.', __file__))
+for testcase in os.listdir(testdata):
+    if not testcase.endswith('.dir'):
+        continue
+    with zipfile.ZipFile(os.path.join(testdata, testcase[:-4]), 'w') as z:
+        PopulateZip(z, os.path.join(testdata, testcase))
